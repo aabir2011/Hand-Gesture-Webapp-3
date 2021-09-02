21d277ed1a10c32b4af6314c6c97f4feeabced15
aabir2011 <80570715+aabir2011@users.noreply.github.com>
2021-09-02 16:39:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:1024
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera');
+function take_snapshot()
+{
+   Webcam.snap(function(data_uri){
+     document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+   });
+}
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/rL3qw08rF/model.json', modelLoaded);
+function modelLoaded() {
+    console.log('Model Loaded')
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "the prediction is " + prediction_1;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis);
+}
+
+  
+  
+    function check()
+    {
+      img = document.getElementById('captured_image');
+      classifier.classify(img, gotResult);
+    }
+  
+  
+  function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      document.getElementById("result_emotion_name").innerHTML = results[0].label;
+      
+      prediction_1 = results[0].label;
+      
+      speak();
+      if(results[0].label == "Peace")
+      {
+          document.getElementById("update_emoji").innerHTML = "✌";
+      }
+      if(results[0].label == "Dislike")
+      {
+          document.getElementById("update_emoji").innerHTML = "👎";
+      }
+      if(results[0].label == "Yo")
+      {
+          document.getElementById("update_emoji").innerHTML = "🤘";
+      }
+      if(results[0].label == "High Five")
+      {
+          document.getElementById("update_emoji").innerHTML = "✋";
+      }
+      if(results[0].label == "Ok")
+      {
+          document.getElementById("update_emoji").innerHTML = "👌";
+      }if(results[0].label == "Call Me")
+      {
+          document.getElementById("update_emoji").innerHTML = "🤙";
+      }if(results[0].label == "Good Job")
+      {
+          document.getElementById("update_emoji").innerHTML = "👍";
+      }
+    }
+  }
+  
